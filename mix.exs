8e24785fbd58bd034a100f833bf96c84b8d0c62d
Thomas Winkler <tom@formrausch.com>
2024-05-06 07:27:24 +0200

    Start a custom nerves base image

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule NervesSystemRpi4.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi4
+  @github_organization "formrausch"
+  @app :frio_rpi4
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
@@ -75,7 +75,7 @@ defmodule NervesSystemRpi4.MixProject do
   end
 
   defp description do
-    "Nerves System - Raspberry Pi 4 (64-bits)"
+    "Formrausch.io Nerves System - Raspberry Pi 4 (64-bits)"
   end
 
   defp docs do
